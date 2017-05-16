e1f65c5e1668c0be72357e20f174016804883f92
Carter Klein <carterklein13@gmail.com>
2017-05-15 22:45:33 -0400

    Initial commit

---
+++ app/src/main/java/carter/muscles/BaseActivity.java
@@ -0,0 +1,38 @@
+package carter.muscles;
+
+/**
+ * Created by Carter Klein on 5/15/2017.
+ */
+
+import android.app.ProgressDialog;
+import android.support.annotation.VisibleForTesting;
+import android.support.v7.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @VisibleForTesting
+    public ProgressDialog mProgressDialog;
+
+    public void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage("Loading");
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        hideProgressDialog();
+    }
+
+}
