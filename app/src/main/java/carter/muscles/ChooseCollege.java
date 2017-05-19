8b28536989eb38a4180ec7ff4fb17b6c30fc4783
Carter Klein <carterklein13@gmail.com>
2017-05-18 22:07:31 -0400

    Just updated my college list to use a ListView like a not huge idiot

--- app/src/main/java/carter/muscles/ChooseCollege.java
+++ app/src/main/java/carter/muscles/ChooseCollege.java
@@ -2,6 +2,8 @@ package carter.muscles;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ChooseCollege extends AppCompatActivity {
 
@@ -9,5 +11,63 @@ public class ChooseCollege extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_college);
+
+        populateListview();
     }
-}
+
+    private void populateListview() {
+        //List of colleges
+        String[] colleges = {
+                //Big 10, SEC, Pac-12 included
+                "University of Alabama",
+                "University of Arizona",
+                "Arizona State University",
+                "Auburn University",
+                "University of Arkansas",
+                "University of California, Berkeley",
+                "University of California, Los Angeles",
+                "University of Colorado, Boulder",
+                "University of Florida",
+                "University of Georgia",
+                "University of Illinois",
+                "Indiana University",
+                "University of Iowa",
+                "University of Kentucky",
+                "Louisiana State University",
+                "University of Maryland",
+                "University of Michigan",
+                "Michigan State University",
+                "University of Minnesota",
+                "University of Mississippi",
+                "Mississippi State University",
+                "University of Missouri",
+                "University of Nebraska",
+                "Northwestern University",
+                "Ohio State University",
+                "University of Oregon",
+                "Oregon State University",
+                "Pennsylvania State University",
+                "Purdue University",
+                "Rutgers University",
+                "University of South Carolina",
+                "University of Southern California",
+                "Stanford University",
+                "University of Tennessee",
+                "Texas A&M University",
+                "University of Utah",
+                "University of Washington",
+                "Washington State University",
+                "THE University of Wisconsin",
+                "Vanderbilt University"};
+
+        //Build Adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,
+                R.layout.colleges_list,
+                colleges);
+
+        //Configure ListView
+        ListView collegeList = (ListView) findViewById(R.id.college_list_view);
+        collegeList.setAdapter(adapter);
+        }
+    }
